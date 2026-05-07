8d5e73f70d799caf62ad4d02f81086dfb5b510ef
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2026-05-07 02:17:26 -0500

    Update build_and_copy.py

--- build_and_copy.py
+++ build_and_copy.py
@@ -1,10 +1,6 @@
 import os
 def compile_exe():
-    choice = input("Build all? y/n:")
-    if (choice == "y"):
-        os.system("build_and_copy.bat all")
-    else:
-        os.system("build_and_copy.bat copy")
+    os.system("build_and_copy.bat")
 
 if __name__ == "__main__":
     compile_exe()
